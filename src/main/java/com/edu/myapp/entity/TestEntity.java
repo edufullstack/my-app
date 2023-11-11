56e337633ff59739e066546735817832b21625c0
37318_230440_jose.ramirezh <jose.ramirezh@coppel.com>
2023-11-11 13:01:53 -0700

    'my-ap'

---
+++ src/main/java/com/edu/myapp/entity/TestEntity.java
@@ -0,0 +1,16 @@
+package com.edu.myapp.entity;
+
+public class TestEntity {
+    /**
+     * Propiedad de prueba para la muestra de compilacion del archivo testentity
+     */
+    private String nombre;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+}
